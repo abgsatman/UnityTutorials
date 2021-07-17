3fde8f32ff9ca3400953c0f8fbeedf1869736009
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-17 15:10:01 +0300

    Target detection

---
+++ Unity Tutorials - 2D/Assets/Scripts/TargetDetection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDetection : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject enemy;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (target.transform.position - enemy.transform.position).normalized;
+        enemy.transform.position += direction * speed * Time.deltaTime;
+    }
+}
