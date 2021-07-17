35de574fd9967bc4c4dd72dca59b3f747c563b07
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-17 16:36:04 +0300

    menu item and some other changes

---
+++ Unity Tutorials - 2D/Assets/Scripts/MenuItemTutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuItemTutorial : MonoBehaviour
+{
+    [MenuItem("HermesGames/Hello Youtube!")]
+    static void HelloYoutube()
+    {
+        Debug.Log("Hello Youtube :)");
+    }
+}
