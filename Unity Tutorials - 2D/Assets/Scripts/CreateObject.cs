9e2c75086f6b145e589aaec4e3bef630c18e24a9
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-17 14:54:54 +0300

    Destroy object and playerprefs

---
+++ Unity Tutorials - 2D/Assets/Scripts/CreateObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    public GameObject obj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateNewObject());
+    }
+
+    IEnumerator CreateNewObject()
+    {
+        while(true)
+        {
+            //randomly
+            Instantiate(obj, new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.75f, 0.75f), 0), Quaternion.identity);
+
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+}
---
+++ Unity Tutorials - 2D/Assets/Scripts/PlayerPrefsTutorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsTutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set variables as int and string
+        if(Input.GetKey("s"))
+        {
+            PlayerPrefs.SetInt("a", 10);
+            PlayerPrefs.SetString("b", "hermes games");
+
+            Debug.Log("set!");
+        }
+
+        //get all of them
+        if(Input.GetKey("g"))
+        {
+            Debug.Log("int: " + PlayerPrefs.GetInt("a"));
+            Debug.Log("string: " + PlayerPrefs.GetString("b"));
+        }
+    }
+}
