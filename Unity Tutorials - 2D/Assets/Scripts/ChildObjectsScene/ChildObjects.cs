24597db361d4aedcb4f13e24ca89438215aaef0f
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-18 21:38:03 +0300

    Itrerate through child objects.

---
+++ Unity Tutorials - 2D/Assets/Scripts/ChildObjectsScene/ChildObjects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            Debug.Log(child.name);
+        }
+    }
+}
