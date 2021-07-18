df1e59ae4afd804628b6df4f6fd1c298085e7638
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-18 18:49:24 +0300

    Debug.Log tutorial.

---
+++ Unity Tutorials - 2D/Assets/Scripts/DebugScene/DebugTutorials.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTutorials : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hermes Games!");
+        Debug.Log("<b>Hermes Games!</b>");
+        Debug.Log("<i>Hermes Games!</i>");
+        Debug.Log("<color=orange>Hermes Games!</color>");
+        Debug.Log("<color=white>Hermes Games!</color>");
+        Debug.Log("<size=45>Hermes Games!</size>");
+        Debug.LogError("Hermes Games!");
+        Debug.LogWarning("Hermes Games!");
+    }
+
+}
