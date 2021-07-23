c82b1aa571c02e8f5ce823331bcb749a1e6cdbd6
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-23 21:25:46 +0300

    Navmesh added.

---
+++ Unity Tutorials - 3D/Assets/Scripts/NavMeshExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshExample : MonoBehaviour
+{
+    public NavMeshAgent player;
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.SetDestination(target.transform.position);
+    }
+}
