39ca93a3aa48c1c8831a0f1b1be414ed4f552746
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-25 02:56:11 +0300

    unity tips: string builder

---
+++ Unity Tutorials - 2D/Assets/Scripts/StringBuilderScene/StringBuilderExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class StringBuilderExample : MonoBehaviour
+{
+    void Start()
+    {
+        //wrong
+        string str = "Youtube Channel!";
+        string content = "welcome to the " + str;
+
+        //correct
+        var sb = new StringBuilder();
+        sb.Append(str);
+        sb.Append(content);
+        sb.Append("lorem ipsum");
+
+        Debug.Log(sb.ToString());
+    }
+}
