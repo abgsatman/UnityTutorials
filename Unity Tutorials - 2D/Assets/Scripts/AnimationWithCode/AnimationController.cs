bf28cee868f85981d9548680265cac5c25e74311
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-08-06 19:48:40 +0300

    Making an animation with coding in Unity

---
+++ Unity Tutorials - 2D/Assets/Scripts/AnimationWithCode/AnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] images;
+
+    [SerializeField]
+    private float speed;
+
+    void Start()
+    {
+        StartCoroutine(AnimationForImages());
+    }
+
+    IEnumerator AnimationForImages()
+    {
+        while(true)
+        {
+            for(int i = 0; i < images.Length; i++)
+            {
+                this.gameObject.GetComponent<Image>().sprite = images[i];
+                yield return new WaitForSeconds(speed);
+            }
+        }
+    }
+}
