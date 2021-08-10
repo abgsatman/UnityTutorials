d51981892a836d60e9b726c74ec8e04ed3cd2617
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-08-10 20:03:10 +0300

    Animation  parameters

---
+++ Unity Tutorials - 2D/Assets/Scripts/AnimationParameters/AnimationParametersExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationParametersExample : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+        animator.SetBool("Idle", false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey("s")) //for stopping
+        {
+            animator.SetBool("Idle", false);
+        }
+
+        if(Input.GetKey("r")) //for running
+        {
+            this.gameObject.GetComponent<Animator>().enabled = true;
+            animator.SetBool("Idle", true);
+        }
+    }
+}
