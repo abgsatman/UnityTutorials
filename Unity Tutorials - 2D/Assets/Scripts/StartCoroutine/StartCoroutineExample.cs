c8be2aca75e8b5d2462acb442ef69f65b3de2460
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-08-15 15:36:01 +0300

    StartCoroutine

---
+++ Unity Tutorials - 2D/Assets/Scripts/StartCoroutine/StartCoroutineExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCoroutineExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitFor());
+    }
+
+    IEnumerator WaitFor()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            Debug.Log("1 second...");
+        }
+    }
+}
