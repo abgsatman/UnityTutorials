d3e7e56534537f45ce860f4857c0883a7fd01238
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-08-22 14:20:09 +0300

    openurl

---
+++ Unity Tutorials - 2D/Assets/Scripts/OpenUrl/ClickUrl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ClickUrl : MonoBehaviour
+{
+    [SerializeField]
+    private string url = "http://hermesgames.com";
+    
+    public void OpenUrl()
+    {
+        Application.OpenURL(url);
+    }
+}
